f7500aedc485c6f86aa35f6abfdcb44ff6052f01
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-06-02 11:58:03 +0200

    simple chat ui

--- client/src/ChatsPage.jsx
+++ client/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-  return <div>ChatsPage</div>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
